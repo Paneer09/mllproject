0af44928ba00fc1f2a6b68d7da3badd64a778936
Paneer09 <paneermars@gmail.com>
2026-03-08 23:22:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# app.py
+
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load saved model
+model = joblib.load("model.joblib")
+
+# App Title
+st.title("🏠 House Price Prediction App")
+
+st.write("Enter house area to predict price")
+
+# User Input
+area = st.number_input("Enter Area (sq ft)", min_value=500, max_value=5000, value=1000)
+
+# Prediction
+if st.button("Predict Price"):
+    prediction = model.predict(np.array([[area]]))
+    st.success(f"Estimated Price: ${prediction[0]:,.2f}")
